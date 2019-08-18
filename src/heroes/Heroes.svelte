13643a6c30f6a46a661f049f342cbc37746627c1
Devlin Duldulao <devlin.duldulao@inmeta.no>
2019-08-19 00:33:54 +0200

    added http service and NewItem Component

--- src/heroes/Heroes.svelte
+++ src/heroes/Heroes.svelte
@@ -1,10 +1,57 @@
 <script>
-    import { onMount } from "svelte";
+    import { onMount } from 'svelte'
+    import Icon from 'fa-svelte'
+    import { faEdit, faEraser } from '@fortawesome/free-solid-svg-icons'
+    import { getHeroes } from './hero-service'
+    import NewItemForm from '../shared/NewItemForm.svelte'
 
-    onMount(() => {});
+    export let heroes = []
+    onMount(async () => {
+    heroes = (await getHeroes()).data
+        console.log(heroes)
+    })
+
+    function handleClickEdit () {
+        alert('handleClickEdit')
+    }
+
+    function handleClickDelete () {
+        alert('handleClickDelete')
+    }
 </script>
 <style>
 
 </style>
 
-<h1>Heroes works!</h1>
+<div class="container-fluid">
+    <NewItemForm ></NewItemForm>
+    {#if heroes.length === 0}
+       <div style="display:flex; flex-direction: row; justify-content: center">
+           <div class="spinner-border" style="width: 9rem; height: 9rem; color: purple" role="status">
+               <span class="sr-only">Loading...</span>
+           </div>
+       </div>
+    {:else}
+        {#each heroes as hero}
+                   <div class="card mt-3" style="width: auto;">
+                        <div class="card-header">
+                            <h3 class="card-title">{ hero.firstName } { hero.lastName }</h3>
+                            <h5 class="card-subtitle mb-2 text-muted">{ hero.house }</h5>
+                            <p class="card-text">{ hero.knownAs }</p>
+                        </div>
+                        <section class="card-body">
+                            <div class="row">
+                                <button on:click={handleClickEdit} class="btn btn-primary card-link col text-center">
+                                    <Icon icon={faEdit} ></Icon>
+                                    Edit
+                                </button>
+                                <button on:click={handleClickDelete} class="btn btn-outline-danger card-link col text-center">
+                                    <Icon icon={faEraser} ></Icon>
+                                    Delete
+                                </button>
+                            </div>
+                        </section>
+                    </div>
+        {/each}
+    {/if}
+</div>
---
+++ src/shared/NewItemForm.svelte
@@ -0,0 +1,73 @@
+<script>
+    import { onMount } from "svelte";
+    import Icon from 'fa-svelte'
+    import { faPlus, faSave, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+    onMount(() => {});
+    function handleClickAdd () {
+        alert('handleClickAdd')
+    }
+    function handleClickCancel () {
+        alert('handleClickCancel')
+    }
+    function handleClickSave () {
+        alert('handleClickSave')
+    }
+</script>
+<style>
+
+</style>
+<section>
+    <div class="d-flex flex-row justify-content-start">
+    <button on:click={handleClickAdd} type="button" class="btn btn-info mx-1">
+        <Icon icon={faPlus} ></Icon> Add New
+    </button>
+
+        {#if true}
+            <button on:click={handleClickCancel} type="button" class="btn btn-outline-warning mx-1">
+                <Icon icon={faChevronLeft} ></Icon> Cancel
+            </button>
+        {/if}
+
+    </div>
+        {#if true}
+                <div class="card my-3" style="width:auto">
+                <form on:submit|preventDefault={handleClickSave} class="card-header">
+                    <section class="d-flex flex-row">
+                        <div class="mt-3 mr-3">
+                            <label for="firstName">First Name</label>
+                            <input name="firstName"
+                                    id="firstName"
+                                    required="required"
+                                    type="text"
+                                    class="form-control" />
+                        </div>
+                        <div class="mt-3 ml-3">
+                            <label for="lastName">Last Name</label>
+                            <input name="lastName"
+                                    id="lastName"
+                                    type="text"
+                                    class="form-control" />
+                        </div>
+                    </section>
+                    <label for="house" class="mt-3">
+                        House
+                    </label>
+                    <input name="house"
+                            id="house"
+                            type="text"
+                            class="form-control" />
+                    <label for="knownAs" class="mt-3">
+                        Known as
+                    </label>
+                    <input name="knownAs"
+                            id="knownAs"
+                            type="text"
+                            class="form-control" />
+                    <button on:click={handleClickSave}  class="btn btn-success mt-3">
+                        <Icon icon={faSave} ></Icon>
+                        Save
+                    </button>
+                </form>
+            </div>
+            {/if}
+</section>
